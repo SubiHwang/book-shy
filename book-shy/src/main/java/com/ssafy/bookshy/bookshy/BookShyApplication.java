a025d9e5a63d956863f4191546d529d2183c04b2
황수비 <ghkdtnql@gmail.com>
2025-04-24 12:51:13 +0900

    👾chore: backend 프로젝트 생성

---
+++ book-shy/src/main/java/com/ssafy/bookshy/bookshy/BookShyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bookshy.bookshy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShyApplication.class, args);
+    }
+
+}
