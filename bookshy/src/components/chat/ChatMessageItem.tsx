22440de670eae6a817fefe8fce82333f278aeacd
김민재 <ourtrip@naver.com>
2025-05-02 14:27:53 +0900

    refactor: 필요없는 코드 삭제

--- bookshy/src/components/chat/ChatMessageItem.tsx
+++ bookshy/src/components/chat/ChatMessageItem.tsx
@@ -6,20 +6,6 @@ interface Props {
 }
 
 function ChatMessageItem({ message, isMyMessage }: Props) {
-  if (message.senderId === 'system') {
-    return (
-      <div className="bg-red-50 text-sm text-light-text-muted rounded-lg p-4 mx-4 my-2 whitespace-pre-line">
-        <p className="text-primary font-bold mb-1">거래 시 주의해주세요!</p>
-        {message.content
-          .split('\n')
-          .slice(1)
-          .map((line, idx) => (
-            <p key={idx}>{line}</p>
-          ))}
-      </div>
-    );
-  }
-
   return (
     <div className={`flex ${isMyMessage ? 'justify-end' : 'justify-start'} px-2 py-1 mb-2`}>
       <div className={`flex ${isMyMessage ? 'flex-row-reverse' : 'flex-row'} items-end gap-2`}>
