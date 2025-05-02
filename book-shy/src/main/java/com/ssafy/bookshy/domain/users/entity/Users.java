44d6e464749714e52d086792e82c9bd336392f3a
황수비 <ghkdtnql@gmail.com>
2025-05-02 16:28:19 +0900

    🔧feat: 회원 entity 구현

---
+++ book-shy/src/main/java/com/ssafy/bookshy/domain/users/entity/Users.java
@@ -0,0 +1,47 @@
+package com.ssafy.bookshy.domain.users.entity;
+
+import com.ssafy.bookshy.common.entity.TimeStampEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class Users extends TimeStampEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "kakao_id", nullable = false)
+    private Long kakaoId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "nickname", nullable = false, length = 100)
+    private String nickname;
+
+    @Column(name = "profile_image_url", columnDefinition = "TEXT")
+    private String profileImageUrl;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private Gender gender;
+
+    private enum Gender{
+        M,F
+    }
+
+
+}
---
+++ book-shy/src/main/java/com/ssafy/bookshy/domain/users/service/UsersService.java
@@ -0,0 +1,7 @@
+package com.ssafy.bookshy.domain.users.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+}
