e234d7fff750b38abd43c848dcf072474b0da022
김민재 <ourtrip@naver.com>
2025-05-02 12:44:24 +0900

    chore: 채팅방 목록에서 임시 프로필 사진 추가

--- bookshy/src/components/chatlist/ChatList.tsx
+++ bookshy/src/components/chatlist/ChatList.tsx
@@ -5,7 +5,8 @@ const dummyChatRooms: ChatRoomSummary[] = [
   {
     roomId: '1',
     partnerName: '마이콜',
-    partnerProfileImage: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbguJOC%2FbtsNFnGX9MK%2F2hVVXFWQM8IwjT1h3vKh8k%2Fimg.jpg',
+    partnerProfileImage:
+      'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbguJOC%2FbtsNFnGX9MK%2F2hVVXFWQM8IwjT1h3vKh8k%2Fimg.jpg',
     lastMessage: '위치가 어디시죠?',
     lastMessageTime: '오후 3:01',
     unreadCount: 2,
@@ -13,7 +14,8 @@ const dummyChatRooms: ChatRoomSummary[] = [
   {
     roomId: '2',
     partnerName: '잭슨',
-    partnerProfileImage: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbzIOjQ%2FbtsNE2iIUCt%2FScKePxVg6XZCFUMPi6KIg0%2Fimg.png',
+    partnerProfileImage:
+      'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbzIOjQ%2FbtsNE2iIUCt%2FScKePxVg6XZCFUMPi6KIg0%2Fimg.png',
     lastMessage: '좋아요. 거래 합시다.',
     lastMessageTime: '오후 3:01',
     unreadCount: 1,
