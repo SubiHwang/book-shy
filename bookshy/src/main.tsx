08ea256d755c6cd83b36a6e40ece6ad3eb9b8b41
yangdahye <yangdahye.e@daum.net>
2025-05-05 04:34:32 +0900

    🐛 fix: 리덕스 Provider 누락 문제 해결

--- bookshy/src/main.tsx
+++ bookshy/src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+import { Provider } from 'react-redux';
+import { store } from './store'; // 정확한 경로 확인 (예: @/store가 아니라면 상대경로로)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
