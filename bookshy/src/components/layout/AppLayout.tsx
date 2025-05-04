64640dd8e764c580802543daac71a41b3f5bb1c3
yangdahye <yangdahye.e@daum.net>
2025-05-05 04:47:54 +0900

    🐛 fix: 내 서재 관련 라우타 수정

--- bookshy/src/components/layout/AppLayout.tsx
+++ bookshy/src/components/layout/AppLayout.tsx
@@ -9,8 +9,11 @@ import TradePromiseList from '@/pages/mypage/TradePromiseList';
 import TradeHistoryList from '@/pages/mypage/TradeHistoryList';
 import WishBooks from '@/pages/matching/WishBooks';
 import NeighborBookshelfPage from '@/pages/matching/NeighborBookshelf/NeighborBookshelfPage';
-import BookSearchPage from '@/pages/mylibrary/BookSearchPage';
-import SelfBookEntryPage from '@/pages/mylibrary/SelfBookEntryPage';
+import AddBySelfPage from '@/pages/mylibrary/AddBook/AddBySelfPage';
+import AddByTitlePage from '@/pages/mylibrary/AddBook/AddByTitlePage';
+import AddByISBNPage from '@/pages/mylibrary/AddBook/AddByISBNPage';
+import AddBySearchPage from '@/pages/mylibrary/AddBook/AddBySearchPage';
+import OCRResultPage from '@/pages/mylibrary/AddBook/OCRResultPage';
 import AllMyBooksTab from '@/pages/mylibrary/tabs/AllBooksTab';
 import PublicMyBooksTab from '@/pages/mylibrary/tabs/PublicBooksTab';
 
@@ -27,16 +30,26 @@ const AppLayout: FC = () => {
       <div className="content">
         <Routes>
           <Route path="/" element={<Navigate to="/bookshelf" replace />} />
+          {/* 📚 서재 기본 페이지 */}
           <Route path="/bookshelf" element={<MyLibraryPage />}>
+            {/* 기본 라우트는 모든 책 페이지로 리다이렉트 */}
             <Route index element={<Navigate to="/bookshelf/all-my-books" replace />} />
+            {/* 책 보기 탭 */}
             <Route path="all-my-books" element={<AllMyBooksTab />} />
             <Route path="public-my-books" element={<PublicMyBooksTab />} />
           </Route>
+
+          {/* 📚 책 추가 페이지들 - 별도 라우트로 분리 */}
+          <Route path="/bookshelf/add/search" element={<AddBySearchPage />} />
+          <Route path="/bookshelf/add/self" element={<AddBySelfPage />} />
+          <Route path="/bookshelf/add/title" element={<AddByTitlePage />} />
+          <Route path="/bookshelf/add/isbn" element={<AddByISBNPage />} />
+          <Route path="/bookshelf/add/ocr-result" element={<OCRResultPage />} />
           <Route path="/matching" element={<MatchingPage />}>
             <Route index element={<MatchingRecommend />} />
             <Route path="wish-books" element={<WishBooks />} />
           </Route>
-            <Route path="matching/neigbors-bookshelf/:userId" element={<NeighborBookshelfPage />} />
+          <Route path="matching/neigbors-bookshelf/:userId" element={<NeighborBookshelfPage />} />
           <Route path="/chat" element={<div>채팅</div>} />
           <Route path="/booknote" element={<div>독서 기록</div>} />
           {/* ✅ 마이페이지 라우팅 추가 */}
@@ -44,11 +57,6 @@ const AppLayout: FC = () => {
             <Route index element={<TradePromiseList />} />
             <Route path="history" element={<TradeHistoryList />} />
           </Route>
-
-          <Route path="/bookshelf/add-by-search" element={<BookSearchPage />} />
-          <Route path="/bookshelf/self-book-entry" element={<SelfBookEntryPage />} />
-          {/* <Route path="/my-library/add-by-title" element={<AddByTitlePage />} />
-          <Route path="/my-library/add-by-isbn" element={<AddByISBNPage />} /> */}
         </Routes>
       </div>
       <BottomTabBar onTabChange={handleTabChange} />
