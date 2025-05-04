e7a06681f7b993d0e53c64952237b1dddc381d03
shSim <sh227sim@gmail.com>
2025-05-04 18:33:38 +0900

    ✨ feat: book 타입 지정

---
+++ bookshy/src/types/book/book.ts
@@ -0,0 +1,12 @@
+export interface Book {
+  bookId: number;
+  title?: string;
+  author?: string;
+  translator?: string;
+  publisher?: string;
+  summary?: string;
+  publishDate?: string;
+  pages?: number;
+  categories?: string;
+  bookImgUrl?: string;
+}
