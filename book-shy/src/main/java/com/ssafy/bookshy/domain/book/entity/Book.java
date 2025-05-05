3702b0e8a956bdf6a69b16da459af0fb302e0cc2
강인혁 <ggh2260@gmail.com>
2025-05-05 21:30:32 +0900

    🚩 Feat: Book 엔티티에 userId 추가

--- book-shy/src/main/java/com/ssafy/bookshy/domain/book/entity/Book.java
+++ book-shy/src/main/java/com/ssafy/bookshy/domain/book/entity/Book.java
@@ -1,5 +1,6 @@
 package com.ssafy.bookshy.domain.book.entity;
 
+import com.ssafy.bookshy.domain.users.entity.Users;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -34,17 +35,26 @@ public class Book {
     private LocalDate pubDate;
     private String coverImageUrl;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Enumerated(EnumType.STRING)
     private Status status;
 
     private LocalDateTime createdAt;
+
     private Integer exchangeCount;
 
+    /**
+     * 📌 현재 이 책을 소유한 사용자 정보
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
     public enum Status {
-        AVAILABLE,
-        EXCHANGING,
-        EXCHANGED
+        AVAILABLE,     // 교환 가능
+        EXCHANGING,    // 교환 중
+        EXCHANGED      // 교환 완료
     }
 }
