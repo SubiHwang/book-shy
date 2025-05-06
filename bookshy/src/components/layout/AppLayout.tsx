f3e66c3f3f2abb4e8ca56b0ef4131e2528f0a94e
김민재 <ourtrip@naver.com>
2025-05-06 19:21:07 +0900

    fix: 채팅창과 리뷰 페이지에서 BottomTabBar 렌더링 되지 않도록 수정

--- bookshy/src/components/layout/AppLayout.tsx
+++ bookshy/src/components/layout/AppLayout.tsx
@@ -69,8 +69,7 @@ const AppLayout: FC = () => {
           </Route>
         </Routes>
       </div>
-      {!isChatRoom && <BottomTabBar onTabChange={handleTabChange} />}
-      {!isReviewPage && <BottomTabBar onTabChange={handleTabChange} />}
+      {!(isChatRoom || isReviewPage) && <BottomTabBar onTabChange={handleTabChange} />}
     </div>
   );
 };
