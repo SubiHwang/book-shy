dc5af8d76c1f03f74b04e23e558744bbed002fa7
shsim <sh227sim@gmail.com>
2025-05-06 22:42:32 +0900

    🐛 fix: 대소문자 수정에 따른 경로 수정

--- bookshy/src/components/matching/MatchRecommend/MatchingList.tsx
+++ bookshy/src/components/matching/MatchRecommend/MatchingList.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { MatchingListProps } from '@/types/Matching';
-import MatchingListCard from '@/components/temp/MatchRecommend/MatchingListCard';
+import MatchingListCard from '@/components/matching/MatchRecommend/MatchingListCard';
 
 const MatchingList: FC<MatchingListProps> = ({ matchings }) => {
   return (
