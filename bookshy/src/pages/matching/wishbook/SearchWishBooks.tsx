19cf38a84e436d60124730b49cfeed0746f1e755
shSim <sh227sim@gmail.com>
2025-05-06 11:48:33 +0900

    ✨ feat: 읽고 싶은 책 검색창 구현

--- bookshy/src/pages/matching/wishbook/SearchWishBooks.tsx
+++ bookshy/src/pages/matching/wishbook/SearchWishBooks.tsx
@@ -1,7 +1,41 @@
+import { ArrowLeft, Search } from 'lucide-react';
+import { useState, KeyboardEvent, ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const SearchWishBooks = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleSearch = (e: KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter') {
+      // api 호출
+      console.log('검색', searchTerm);
+      setSearchTerm('');
+    }
+  };
   return (
     <div>
-      <p>읽고 싶은 책 검색 하기</p>
+      <div className="bg-primary-light px-4 py-3">
+        <div>
+          <button onClick={() => navigate(-1)} className="mr-4 p-1 text-white" aria-label="Go back">
+            <ArrowLeft size={24} />
+          </button>
+        </div>
+        <p className="text-xl font-light text-white text-center mb-3">읽고 싶은 책을 검색 해보세요.</p>
+        <div className="relative flex items-center w-full">
+          <input
+            type="text"
+            placeholder="책 제목, 작가..."
+            value={searchTerm}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+            onKeyDown={handleSearch}
+            className="w-full px-4 py-2 pr-10 rounded-full border-none focus:outline-none shadow-sm text-gray-800"
+          />
+          <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+            <Search size={20} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
