c84df5232880835e70584d5c737256181b8727bc
황수비 <ghkdtnql@gmail.com>
2025-05-07 09:33:38 +0900

    🔧feat: swagger cors 추가

--- book-shy/src/main/java/com/ssafy/bookshy/common/config/SecurityConfig.java
+++ book-shy/src/main/java/com/ssafy/bookshy/common/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.ssafy.bookshy.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource((corsConfigurationSource)))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/v3/api-docs/**",
@@ -25,7 +30,7 @@ public class SecurityConfig {
 
                         ).permitAll() // Swagger 접근 허용
                         .anyRequest().permitAll() // 💡 전체 요청 허용 (로컬 테스트용)
-                ).csrf(csrf -> csrf.disable());
+                );
 
         return http.build();
     }
