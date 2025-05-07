b678f5d4e67f0d26bb9dc54c9db0a6196408a3ae
황수비 <ghkdtnql@gmail.com>
2025-05-07 13:29:32 +0900

    Revert "Merge branch 'S12P31D204-70-be-회원' into 'master'"
    This reverts merge request !27

--- book-shy/src/main/java/com/ssafy/bookshy/domain/booknote/repository/BookNoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.bookshy.domain.booknote.repository;
-
-import com.ssafy.bookshy.domain.booknote.entity.BookNote;
-import com.ssafy.bookshy.domain.booknote.entity.BookQuote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookNoteRepository extends JpaRepository<BookNote, Long> {}
-
--- book-shy/src/main/java/com/ssafy/bookshy/domain/booknote/repository/BookQuoteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.bookshy.domain.booknote.repository;
-
-import com.ssafy.bookshy.domain.booknote.entity.BookQuote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookQuoteRepository extends JpaRepository<BookQuote, Long> {}
