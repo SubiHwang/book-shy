03e0c3063da6c545aa888de656c0d196ddbd32fe
강인혁 <ggh2260@gmail.com>
2025-05-08 11:10:11 +0900

    🔧 Chore: Kafka Producer Config 누락 필드 추가

--- book-shy/src/main/java/com/ssafy/bookshy/kafka/config/KafkaConfig.java
+++ book-shy/src/main/java/com/ssafy/bookshy/kafka/config/KafkaConfig.java
@@ -39,6 +39,7 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.bootstrap-servers"));
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false); // 타입 헤더 생략
         return props;
     }
