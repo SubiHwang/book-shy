979f0067210026f2f59a9bd8d9193aa1171b3409
Sunhwa Sim <sh227sim@gmail.com>
2025-05-08 16:14:53 +0900

    ✨ feat: 로그인 시 로딩 로직 및 splash 화면 추가 , 로그인 성공 여부에 따른 리다이렉트 설정

--- bookshy/src/pages/auth/KaKaoOauth.tsx
+++ bookshy/src/pages/auth/KaKaoOauth.tsx
@@ -1,24 +1,66 @@
-import { useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
+import { useFirebase } from '@/contexts/FirebaseContext';
+import SplashScreen from '@/components/splash/SplashScreen';
+import bookAnimation from '@/assets/lottie/bookshy-splash.json';
 
-const KaKaoOauth = () => {
-  const { login } = useAuth(); // AuthContext에서 login 함수 가져오기
+const KaKaoOauth: FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { login, isLoggedIn } = useAuth();
+  const { firebaseToken, isInitialized } = useFirebase();
   const location = useLocation();
   const navigate = useNavigate();
-  const fcmToken = 'your-fcm-token'; // FCM 토큰을 여기에 설정하세요.
 
   useEffect(() => {
-    // URL에서 인가 코드 추출
-    const code = new URLSearchParams(location.search).get('code');
-
-    if (code) {
-      console.log('인가 코드 추출 성공:', code);
-      login({ token: code, fcmToken: fcmToken }); // 인가 코드로 로그인 시도
-    } else {
-      console.error('인가 코드가 없습니다.');
-      navigate('/login'); // 코드가 없으면 로그인 페이지로 리다이렉트
+    // 로그인 상태 확인 및 스플래시 화면이 끝났을 때
+    if (!isLoading && isLoggedIn) {
+      navigate('/');
+    } else if (!isLoading && !isLoggedIn) {
+      // 로그인하지 않은 상태에서 스플래시 화면이 끝났을 때
+      navigate('/login');
     }
-  }, [location, navigate, login, fcmToken]);
+  }, [isLoading, isLoggedIn, navigate]);
+
+  useEffect(() => {
+    const loginWithToken = async () => {
+      const code = new URLSearchParams(location.search).get('code');
+
+      if (!code) {
+        console.error('인가 코드가 없습니다.');
+        navigate('/login');
+        return;
+      }
+
+      if (!isInitialized) return;
+
+      try {
+        login({
+          token: code,
+          fcmToken: firebaseToken || '',
+        });
+      } catch (error) {
+        console.error('로그인 처리 중 오류:', error);
+        navigate('/login');
+      }
+    };
+
+    loginWithToken();
+  }, [location, navigate, login, firebaseToken, isInitialized]);
+
+  // 스플래시 화면 종료 처리
+  const handleSplashFinished = (): void => {
+    setIsLoading(false);
+  };
+
+  return (
+    <SplashScreen
+      animationData={bookAnimation}
+      onFinish={handleSplashFinished}
+      minDisplayTime={2500}
+      text="북끄북끄"
+    />
+  );
 };
+
 export default KaKaoOauth;
