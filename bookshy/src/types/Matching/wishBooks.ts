b43015b56322ad97c1c6ec00c6a70caa8f560c8d
shsim <sh227sim@gmail.com>
2025-05-08 22:15:51 +0900

    ✨ fix: wishbooks 타입 변경 및 api 응답 타입 추가

--- bookshy/src/types/Matching/wishBooks.ts
+++ bookshy/src/types/Matching/wishBooks.ts
@@ -3,9 +3,18 @@ import { WishBook } from '../book';
 
 export interface SearchBarProps {
   onSearch: (e: KeyboardEvent<HTMLInputElement>) => void;
+  value: string;
+  onChange: (value: string) => void;
 }
 
 export interface SearchResultListProps {
   resultList: WishBook[];
   searchTerm: string;
+  total: number;
+  isLoading: boolean;
+}
+
+export interface SearchResultResponse {
+  total: number;
+  books: WishBook[];
 }
