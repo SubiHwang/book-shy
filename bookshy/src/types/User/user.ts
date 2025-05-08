122dbc88e259ad9d03c0bb0d3e7c1705849091e5
심순화 <sh227sim@gmail.com>
2025-05-08 18:01:24 +0900

    Revert "Merge branch 'feat#15-회원관리' into 'master'"
    This reverts merge request !34

--- bookshy/src/types/User/user.ts
+++
@@ -1,6 +0,0 @@
-export interface UserProfile {
-  nickname: string;
-  profileImageUrl: string;
-  bookShyScore: number; // ⬅️ 이름 변경 및 float 허용
-  badge: string;
-}
--- bookshy/src/types/splash/components.ts
+++
@@ -1,6 +0,0 @@
-export interface SplashScreenProps {
-    animationData: any; // Lottie JSON 데이터
-    onFinish: () => void;
-    minDisplayTime?: number;
-    text?: string;
-  }
