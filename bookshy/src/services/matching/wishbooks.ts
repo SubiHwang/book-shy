0b498c27cf1ce76d0555a284e126bd974e6db469
shsim <sh227sim@gmail.com>
2025-05-08 23:01:54 +0900

    ✨ feat: 읽고 싶은 책 목록 불러오기 api 함수 구현

--- bookshy/src/services/matching/wishbooks.ts
+++ bookshy/src/services/matching/wishbooks.ts
@@ -1,16 +1,28 @@
-import { SearchResultResponse } from '@/types/Matching/wishBooks';
+import { WishBooksResponse } from '@/types/Matching/wishBooks';
 import { authAxiosInstance } from '../axiosInstance';
 
-
-export const getSearchResult = async (searchTerm: string): Promise<SearchResultResponse> => {
+export const getSearchResult = async (searchTerm: string): Promise<WishBooksResponse> => {
   try {
-    const response = await authAxiosInstance.get<string, SearchResultResponse>(
+    const response = await authAxiosInstance.get<string, WishBooksResponse>(
       `book/search/list?q=${searchTerm}`,
     );
     console.log('읽고 싶은 책 검색 API 응답:', response);
     return response;
   } catch (error) {
     console.error('읽고 싶은 책 검색 API 호출 중 오류 발생:', error);
+    throw error; // 오류를 다시 던져서 호출한 곳에서 처리할 수 있도록 함
+  }
+};
+
+export const getWishBookList = async (userId: number): Promise<WishBooksResponse> => {
+  try {
+    const response = await authAxiosInstance.get<string, WishBooksResponse>(
+      `/book/wish?userId=${userId}`,
+    );
+    console.log('읽고 싶은 책 목록 API 응답:', response);
+    return response;
+  } catch (error) {
+    console.error('읽고 싶은 책 목록 API 호출 중 오류 발생:', error);
     throw error; // 오류를 다시 던져서 호출한 곳에서 처리할 수 있도록 함
   }
 };
