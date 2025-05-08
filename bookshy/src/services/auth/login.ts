881ffc565f82389eb896fdeedb57fcf282e8dbc0
Sunhwa Sim <sh227sim@gmail.com>
2025-05-08 17:59:59 +0900

    🐛 fix: 로그인 api 함수 타입 지정

--- bookshy/src/services/auth/login.ts
+++ bookshy/src/services/auth/login.ts
@@ -16,15 +16,18 @@ export const kakaoLogin = async ({
 }: KakaoLoginRequest): Promise<KakaoLoginResponse> => {
   try {
     // POST 요청의 형식에 맞게 수정
-    const response = await publicAxiosInstance.post('/auth/sign-in/kakao', {
-      token: token,
-      fcmToken: fcmToken,
-    });
+    const res = await publicAxiosInstance.post<KakaoLoginRequest, KakaoLoginResponse>(
+      '/auth/sign-in/kakao',
+      {
+        token: token,
+        fcmToken: fcmToken,
+      },
+    );
 
-    console.log('카카오 로그인 응답:', response);
+    console.log('카카오 로그인 응답:', res);
 
     // 응답 데이터 반환
-    return response.data;
+    return res;
   } catch (error) {
     console.error('카카오 로그인 오류:', error);
     throw error;
