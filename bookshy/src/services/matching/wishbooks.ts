efa9ec35b82e2311c9d697a74139a2250f193b85
shsim <sh227sim@gmail.com>
2025-05-10 01:38:25 +0900

    🐛 fix: 읽고 싶은 책 좋아요, 취소 api 수정에 따른 api 호츌 함수 및 함수 사용 수정

--- bookshy/src/services/matching/wishbooks.ts
+++ bookshy/src/services/matching/wishbooks.ts
@@ -27,10 +27,10 @@ export const getWishBookList = async (): Promise<WishBooksResponse> => {
   }
 };
 
-export const addWishBook = async (userId: number, itemId: number): Promise<boolean> => {
+export const addWishBook = async (itemId: number): Promise<boolean> => {
   try {
     const response = await authAxiosInstance.post<string, boolean>(
-      `/book/wish?userId=${userId}&itemId=${itemId}`,
+      `/book/wish?itemId=${itemId}`,
     );
     console.log('읽고 싶은 책 추가 API 응답:', response);
     return response;
@@ -40,10 +40,10 @@ export const addWishBook = async (userId: number, itemId: number): Promise<boole
   }
 };
 
-export const deleteWishBook = async (userId: number, itemId: number): Promise<boolean> => {
+export const deleteWishBook = async (itemId: number): Promise<boolean> => {
   try {
     const response = await authAxiosInstance.delete<string, boolean>(
-      `/book/wish/remove?userId=${userId}&itemId=${itemId}`,
+      `/book/wish/remove?itemId=${itemId}`,
     );
     console.log('읽고 싶은 책 삭제 API 응답:', response);
     return response;
