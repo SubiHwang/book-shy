5ec87a45e48a325d50e98aad83914af742e05e25
shSim <sh227sim@gmail.com>
2025-05-10 17:41:12 +0900

    🐛 fix: wishbooks 리액트 쿼리 내보내기 변경

--- bookshy/src/hooks/wishbook/useWishBooks.tsx
+++ bookshy/src/hooks/wishbook/useWishBooks.tsx
@@ -2,9 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { getWishBookList } from '@/services/matching/wishbooks';
 import { WishBooksResponse } from '@/types/Matching';
 
-export const useWishBooks = () => {
+const useWishBooks = () => {
   return useQuery<WishBooksResponse, Error>({
     queryKey: ['wishBooks'],
     queryFn: () => getWishBookList(),
   });
 };
+
+export default useWishBooks;
