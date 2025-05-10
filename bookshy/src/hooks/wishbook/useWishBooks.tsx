f9d69dabd19a67bdff273c7e36bafcbaee303cfb
shSim <sh227sim@gmail.com>
2025-05-11 00:31:11 +0900

    🐛 fix: useWishbooks export 방식 수정

--- bookshy/src/hooks/wishbook/useWishBooks.tsx
+++ bookshy/src/hooks/wishbook/useWishBooks.tsx
@@ -2,11 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import { getWishBookList } from '@/services/matching/wishbooks';
 import { WishBooksResponse } from '@/types/Matching';
 
-const useWishBooks = () => {
+export const useWishBooks = () => {
   return useQuery<WishBooksResponse, Error>({
     queryKey: ['wishBooks'],
     queryFn: () => getWishBookList(),
   });
 };
-
-export default useWishBooks;
