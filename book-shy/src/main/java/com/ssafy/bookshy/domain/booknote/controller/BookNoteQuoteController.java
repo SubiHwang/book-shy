b2921075789a03f1ec59e3c4207c3ac812058cb8
강인혁 <ggh2260@gmail.com>
2025-05-10 20:37:49 +0900

    🐛 Fix: 독서기록 컨트롤러 스웨거 인자 에러 수정

--- book-shy/src/main/java/com/ssafy/bookshy/domain/booknote/controller/BookNoteQuoteController.java
+++ book-shy/src/main/java/com/ssafy/bookshy/domain/booknote/controller/BookNoteQuoteController.java
@@ -6,11 +6,11 @@ import com.ssafy.bookshy.domain.booknote.dto.BookNoteQuoteUpdateRequest;
 import com.ssafy.bookshy.domain.booknote.service.BookNoteQuoteService;
 import com.ssafy.bookshy.domain.users.entity.Users;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,21 +29,19 @@ public class BookNoteQuoteController {
     @Operation(
             summary = "📝✍️ 독후감 + 인용구 등록",
             description = """
-                    사용자가 <b>하나의 요청으로 독후감과 인용구를 동시에 등록</b>할 수 있습니다.<br><br>
-                    등록된 독후감은 <code>book_reviews</code> 테이블에,<br>
-                    인용구는 <code>book_quotes</code> 테이블에 각각 저장됩니다.<br><br>
-                    만약 도서 정보가 없다면 <b>books 테이블에 도서가 자동 등록</b>된 후 진행됩니다.
-                    """,
+                🔒 <b>로그인 사용자</b>의 인증 정보를 기반으로 독후감과 인용구를 동시에 등록합니다.<br><br>
+                - 독후감은 <code>book_reviews</code>, 인용구는 <code>book_quotes</code> 테이블에 저장됩니다.<br>
+                - 도서가 존재하지 않으면 <b>자동으로 등록</b>됩니다.
+                """,
             requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    description = "작성자 ID, 도서 ID, 독후감 내용, 인용 문장을 모두 포함합니다.",
+                    description = "도서 ID, 독후감 내용, 인용 문장을 포함합니다.",
                     required = true,
                     content = @Content(
                             mediaType = "application/json",
                             examples = @ExampleObject(
-                                    name = "독후감 + 인용구 등록 예시",
+                                    name = "등록 예시",
                                     value = """
                                             {
-                                              "userId": 1,
                                               "bookId": 101,
                                               "reviewContent": "이 책은 삶의 균형에 대해 많은 통찰을 줍니다.",
                                               "quoteContent": "모든 사람은 자신의 이야기의 주인공이다."
@@ -58,8 +56,10 @@ public class BookNoteQuoteController {
                     @ApiResponse(responseCode = "500", description = "💥 서버 내부 오류")
             }
     )
-    public ResponseEntity<BookNoteQuoteResponse> createNoteAndQuote(@RequestBody BookNoteQuoteRequest request,
-                                                                    @AuthenticationPrincipal Users user) {
+    public ResponseEntity<BookNoteQuoteResponse> createNoteAndQuote(
+            @RequestBody BookNoteQuoteRequest request,
+            @AuthenticationPrincipal Users user
+    ) {
         request.setUserId(user.getUserId()); // ✅ 인증된 사용자 ID 설정
         BookNoteQuoteResponse response = bookNoteQuoteService.registerNoteAndQuote(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -91,5 +91,4 @@ public class BookNoteQuoteController {
         BookNoteQuoteResponse response = bookNoteQuoteService.updateNoteAndQuote(reviewId, quoteId, request);
         return ResponseEntity.ok(response);
     }
-
 }
