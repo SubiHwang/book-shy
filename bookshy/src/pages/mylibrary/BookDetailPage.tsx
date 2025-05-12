f0a1958b35461da7525719924bc2d98013c404c5
yangdahye <yangdahye.e@daum.net>
2025-05-12 09:48:19 +0900

    💄 fix: 상세페이지 헤더 부분 공통 컴포넌트 배경색 적용

--- bookshy/src/pages/mylibrary/BookDetailPage.tsx
+++ bookshy/src/pages/mylibrary/BookDetailPage.tsx
@@ -156,7 +156,7 @@ const BookDetailPage: React.FC = () => {
         title="도서 상세 보기"
         showBackButton={true}
         showNotification={true} // 알림 아이콘 표시
-        className="bg-gradient-to-r from-[#FCF6D4] to-[#F4E8B8] shadow-none"
+        className="bg-light-bg shadow-md"
         onBackClick={() => navigate(-1)}
       />
       <div className="bookshelf-container flex flex-col min-h-screen">
