6887028b7b8833902461bd3dcde3d6ba072da027
yangdahye <yangdahye.e@daum.net>
2025-05-15 17:55:46 +0900

    💄 fix: 버튼 내용 수정 (공개하기, 숨기기)

--- bookshy/src/components/mylibrary/BookDetail/BookDetailHeader.tsx
+++ bookshy/src/components/mylibrary/BookDetail/BookDetailHeader.tsx
@@ -81,16 +81,16 @@ const BookDetailHeader: React.FC<BookDetailHeaderProps> = ({
             </div>
 
             {!isLoading && (
-              <div className="flex space-x-2 mt-3">
+              <div className="flex flex-wrap space-x-2 mt-3">
                 <button
-                  className="py-1.5 px-4 rounded-full bg-white bg-opacity-70 hover:bg-opacity-90 shadow-sm text-gray-700 text-sm font-medium transition-colors"
+                  className="py-1.5 px-3 rounded-full bg-white bg-opacity-70 hover:bg-opacity-90 shadow-sm text-gray-700 text-sm font-medium transition-colors whitespace-nowrap"
                   onClick={onTogglePublicStatus}
                 >
-                  {bookDetail.isPublic ? '공개 서재에서 숨기기' : '공개 서재에 추가'}
+                  {bookDetail.isPublic ? '숨기기' : '공개하기'}
                 </button>
 
                 <button
-                  className="py-1.5 px-4 rounded-full bg-white bg-opacity-70 hover:bg-opacity-90 shadow-sm text-gray-700 text-sm font-medium transition-colors"
+                  className="py-1.5 px-3 rounded-full bg-white bg-opacity-70 hover:bg-opacity-90 shadow-sm text-gray-700 text-sm font-medium transition-colors whitespace-nowrap"
                   onClick={onDelete}
                 >
                   삭제하기
